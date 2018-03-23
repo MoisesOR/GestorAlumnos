8c91e02de78a9ada1829f9af0c3d91232448e621
MoisesOR <moises.ortega@atmira.com>
2018-03-23 23:29:31 +0100

    Subida de todo el proyecto

---
+++ GestionAlumnos/Alumno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionAlumnos
+{
+    public class Alumno :IAlumno
+    {
+        public string Apellido(string apellido)
+        {
+            return apellido;
+        }
+
+        public string Dni(string dni)
+        {
+            return dni;
+        }
+
+        public string Id(string id)
+        {
+            return id;
+        }
+
+        public string Nombre(string nombre)
+        {
+            return nombre;
+        }
+    }
+}
---
+++ GestionAlumnos/IAlumno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionAlumnos
+{
+    public interface IAlumno
+    {
+        string Id(string id);
+        string Nombre(string nombre);
+        string Apellido(string apellido);
+        string Dni(string dni);
+    }
+}
